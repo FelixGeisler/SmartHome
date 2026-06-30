ea93a5ab6bdb7fcfcf124b20c3ee8f77e57f72f1
Felix <felixgeisler7474@gmail.com>
2026-07-01 00:14:44 +0200

    #47 Make the hub-settings entity and repository public, matching the other Spring Data types.

--- src/main/java/org/felixgeisler/smarthome/settings/HubSetting.java
+++ src/main/java/org/felixgeisler/smarthome/settings/HubSetting.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
  */
 @Entity
 @Table(name = "hub_setting")
-class HubSetting {
+public class HubSetting {
 
   @Id
   @Column(name = "setting_key")
--- src/main/java/org/felixgeisler/smarthome/settings/HubSettingRepository.java
+++ src/main/java/org/felixgeisler/smarthome/settings/HubSettingRepository.java
@@ -3,4 +3,4 @@ package org.felixgeisler.smarthome.settings;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /** Spring Data repository for persisted {@link HubSetting} values, keyed by setting name. */
-interface HubSettingRepository extends JpaRepository<HubSetting, String> {}
+public interface HubSettingRepository extends JpaRepository<HubSetting, String> {}
